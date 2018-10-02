9f569bbca077166561d01f6b7b05ad68520e8581
Czerny-F <dummy00.cz@gmail.com>
2018-10-02 18:19:09 +0900

    dispatch/subscript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { NgRedux } from '@angular-redux/store'; // <- New
+import { CounterActions } from './app.actions'; // <- New
+import { IAppState } from "../store"; // <- New
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'angular-redux-quickstart';
   count: number;
+  subscription;
 
-  increment() {}
+  constructor(
+    private ngRedux: NgRedux<IAppState>,
+    private actions: CounterActions) {
+    this.subscription = ngRedux.select<number>('count')
+      .subscribe(newCount => this.count = newCount);
+  }
 
-  decrement() {}
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  increment() {
+    this.ngRedux.dispatch(this.actions.increment());
+  }
+
+  decrement() {
+    this.ngRedux.dispatch(this.actions.decrement());
+  }
 }
