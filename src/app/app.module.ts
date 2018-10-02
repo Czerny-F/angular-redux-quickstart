651dfea6483b2f1e23d1f6127e1b7f7b46f06b4a
Czerny-F <dummy00.cz@gmail.com>
2018-10-02 18:13:29 +0900

    actions/store(state/reducer)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgReduxModule, NgRedux } from '@angular-redux/store';
 
 import { AppComponent } from './app.component';
 
+import { rootReducer, IAppState, INITIAL_STATE } from '../store';
+import { CounterActions } from './app.actions';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,16 @@ import { AppComponent } from './app.component';
     BrowserModule,
     NgReduxModule,
   ],
-  providers: [],
+  providers: [CounterActions],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    // Tell @angular-redux/store about our rootReducer and our initial state.
+    // It will use this to create a redux store for us and wire up all the
+    // events.
+    ngRedux.configureStore(
+      rootReducer,
+      INITIAL_STATE);
+  }
+}
